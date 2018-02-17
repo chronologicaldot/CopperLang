803e87da7e878ce841d0d66f25c9b43b15aeb193
chronologicaldot <anderocketech@gmail.com>
2018-02-16 20:26:33 -0600

    Cleanup of math section (files deleted)

--- exts/Math/tests/float_compare.cu
+++
@@ -1,3 +0,0 @@
-assert( float_gte(float(1) float(0.5)) )
-assert( float_lte(float(1) float(2)) )
-assert( float_equal(float(0.5) float(0.5)) )
--- exts/Math/tests/int_compare.cu
+++
@@ -1,3 +0,0 @@
-assert( int_gte(int(2) int(1)) )
-assert( int_lte(int(1) int(2)) )
-assert( int_equal(int(4) int(4)) )
